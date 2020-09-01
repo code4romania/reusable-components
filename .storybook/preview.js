c5fbc5fa765fc95f6e57849d08c692aea449d7da
Marius Petcu <dapetcu21@gmail.com>
2020-09-01 15:29:46 +0300

    Add font to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,8 @@ import '@storybook/addon-console'
 import { addDecorator } from '@storybook/react';
 import { withConsole } from '@storybook/addon-console';
 
+import "./storybook.scss";
+
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 
 export const parameters = {
