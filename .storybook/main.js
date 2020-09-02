99c3c9b0aaf901b386d7ed79f45534e1959f1050
Marius Petcu <dapetcu21@gmail.com>
2020-09-02 13:14:14 +0300

    Fix module CSS in storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/react",
-    "@storybook/preset-scss",
+    {
+      name: "@storybook/preset-scss",
+      options: { cssLoaderOptions: { modules: { auto: true } } },
+    },
     "@storybook/addon-storysource",
     "@storybook/addon-knobs/register",
-  ]
+  ],
 };
