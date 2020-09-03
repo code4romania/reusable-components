7a991ecf013d3f325161602b752c21da55f3256b
Marius Petcu <dapetcu21@gmail.com>
2020-09-03 12:26:31 +0300

    Split up ElectionTurnoutSection

--- src/components/ElectionTurnoutSection/ElectionTurnoutSection.tsx
+++ src/components/ElectionTurnoutSection/ElectionTurnoutSection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { DIASPORA, ElectionMeta, ElectionScope, ElectionTurnout } from "../../types/Election";
-import { formatGroupedNumber, formatPercentage } from "../../util/format";
+import { ElectionMeta, ElectionScope, ElectionTurnout } from "../../types/Election";
+import { formatGroupedNumber, formatPercentage, getScopeName } from "../../util/format";
 import { themable, useTheme } from "../../util/theme";
 import { PercentageBars } from "../PercentageBars/PercentageBars";
 import { PercentageBarsLegend } from "../PercentageBarsLegend/PercentageBarsLegend";
@@ -13,47 +13,34 @@ type Props = {
   turnout?: ElectionTurnout;
 };
 
-function getScopeName(scope: ElectionScope) {
-  switch (scope.type) {
-    case "national":
-      return "Nivel Național";
-    case "county":
-      return scope.county === DIASPORA ? "Diaspora" : `Județul ${scope.county}`;
-    case "uat":
-      return scope.county === DIASPORA
-        ? `Diaspora din ${scope.uat}`
-        : `Unitatea administrativ-teritorială ${scope.uat}`;
-    case "city":
-      return `Localitatea ${scope.city}`;
-  }
-}
-
-const ElectionTurnoutBars = ({ classes, eligibleVoters, totalVotes }) => {
-  const theme = useTheme();
-  const voterRatio = totalVotes / eligibleVoters;
-  const items = [
-    {
-      color: theme.colors.primary,
-      legendName: "Cetățeni cu drept de vot",
-      value: 1,
-      valueLabel: formatPercentage(1),
-      legendNote: `(${formatGroupedNumber(eligibleVoters)})`,
-    },
-    {
-      color: theme.colors.secondary,
-      legendName: "Au votat",
-      value: voterRatio,
-      valueLabel: formatPercentage(voterRatio),
-      legendNote: `(${formatGroupedNumber(totalVotes)})`,
-    },
-  ];
-  return (
-    <>
-      <PercentageBars className={classes.percentageBars} total={1} items={items} />
-      <PercentageBarsLegend items={items} />
-    </>
-  );
-};
+export const ElectionTurnoutBars = themable<{ eligibleVoters: number; totalVotes: number }>("ElectionTurnoutBars")(
+  ({ classes, eligibleVoters, totalVotes }) => {
+    const theme = useTheme();
+    const voterRatio = totalVotes / eligibleVoters;
+    const items = [
+      {
+        color: theme.colors.primary,
+        legendName: "Cetățeni cu drept de vot",
+        value: 1,
+        valueLabel: formatPercentage(1),
+        legendNote: `(${formatGroupedNumber(eligibleVoters)})`,
+      },
+      {
+        color: theme.colors.secondary,
+        legendName: "Au votat",
+        value: voterRatio,
+        valueLabel: formatPercentage(voterRatio),
+        legendNote: `(${formatGroupedNumber(totalVotes)})`,
+      },
+    ];
+    return (
+      <div className={classes.root}>
+        <PercentageBars total={1} items={items} />
+        <PercentageBarsLegend items={items} />
+      </div>
+    );
+  },
+);
 
 export const ElectionTurnoutSection = themable<Props>(
   "ElectionTurnoutSection",
@@ -71,7 +58,7 @@ export const ElectionTurnoutSection = themable<Props>(
         )}
         {turnout && turnout.eligibleVoters != null && (
           <ElectionTurnoutBars
-            classes={classes}
+            className={classes.percentageBars}
             eligibleVoters={turnout.eligibleVoters}
             totalVotes={turnout.totalVotes}
           />
--- src/util/format.ts
+++ src/util/format.ts
@@ -1,3 +1,5 @@
+import { DIASPORA, ElectionScope } from "../types/Election";
+
 export const formatPercentage = (x: number): string =>
   new Intl.NumberFormat("ro-RO", {
     style: "percent",
@@ -8,3 +10,18 @@ export const formatGroupedNumber = (x: number): string =>
   new Intl.NumberFormat("ro-RO", {
     useGrouping: true,
   }).format(x);
+
+export function getScopeName(scope: ElectionScope): string {
+  switch (scope.type) {
+    case "national":
+      return "Nivel Național";
+    case "county":
+      return scope.county === DIASPORA ? "Diaspora" : `Județul ${scope.county}`;
+    case "uat":
+      return scope.county === DIASPORA
+        ? `Diaspora din ${scope.uat}`
+        : `Unitatea administrativ-teritorială ${scope.uat}`;
+    case "city":
+      return `Localitatea ${scope.city}`;
+  }
+}
