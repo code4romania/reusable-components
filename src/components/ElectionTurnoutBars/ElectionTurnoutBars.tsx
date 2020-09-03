25d5812e4fbfeb483c4c4c0a849df85220908d56
Marius Petcu <dapetcu21@gmail.com>
2020-09-03 12:30:35 +0300

    Move ElectionTurnoutBars to its own file

---
+++ src/components/ElectionTurnoutBars/ElectionTurnoutBars.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { formatGroupedNumber, formatPercentage } from "../../util/format";
+import { themable, useTheme } from "../../util/theme";
+import { PercentageBars } from "../PercentageBars/PercentageBars";
+import { PercentageBarsLegend } from "../PercentageBarsLegend/PercentageBarsLegend";
+
+type Props = {
+  eligibleVoters: number;
+  totalVotes: number;
+};
+
+export const ElectionTurnoutBars = themable<Props>("ElectionTurnoutBars")(({ classes, eligibleVoters, totalVotes }) => {
+  const theme = useTheme();
+  const voterRatio = totalVotes / eligibleVoters;
+  const items = [
+    {
+      color: theme.colors.primary,
+      legendName: "Cetățeni cu drept de vot",
+      value: 1,
+      valueLabel: formatPercentage(1),
+      legendNote: `(${formatGroupedNumber(eligibleVoters)})`,
+    },
+    {
+      color: theme.colors.secondary,
+      legendName: "Au votat",
+      value: voterRatio,
+      valueLabel: formatPercentage(voterRatio),
+      legendNote: `(${formatGroupedNumber(totalVotes)})`,
+    },
+  ];
+  return (
+    <div className={classes.root}>
+      <PercentageBars total={1} items={items} />
+      <PercentageBarsLegend items={items} />
+    </div>
+  );
+});
--- src/components/ElectionTurnoutSection/ElectionTurnoutSection.tsx
+++ src/components/ElectionTurnoutSection/ElectionTurnoutSection.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { ElectionMeta, ElectionScope, ElectionTurnout } from "../../types/Election";
-import { formatGroupedNumber, formatPercentage, getScopeName } from "../../util/format";
-import { themable, useTheme } from "../../util/theme";
-import { PercentageBars } from "../PercentageBars/PercentageBars";
-import { PercentageBarsLegend } from "../PercentageBarsLegend/PercentageBarsLegend";
+import { getScopeName } from "../../util/format";
+import { themable } from "../../util/theme";
+import { ElectionTurnoutBars } from "../ElectionTurnoutBars/ElectionTurnoutBars";
 import { BodyHuge, Heading2, Label } from "../Typography/Typography";
 import cssClasses from "./ElectionTurnoutSection.module.scss";
 
@@ -12,35 +11,6 @@ type Props = {
   scope: ElectionScope;
   turnout?: ElectionTurnout;
 };
-
-export const ElectionTurnoutBars = themable<{ eligibleVoters: number; totalVotes: number }>("ElectionTurnoutBars")(
-  ({ classes, eligibleVoters, totalVotes }) => {
-    const theme = useTheme();
-    const voterRatio = totalVotes / eligibleVoters;
-    const items = [
-      {
-        color: theme.colors.primary,
-        legendName: "Cetățeni cu drept de vot",
-        value: 1,
-        valueLabel: formatPercentage(1),
-        legendNote: `(${formatGroupedNumber(eligibleVoters)})`,
-      },
-      {
-        color: theme.colors.secondary,
-        legendName: "Au votat",
-        value: voterRatio,
-        valueLabel: formatPercentage(voterRatio),
-        legendNote: `(${formatGroupedNumber(totalVotes)})`,
-      },
-    ];
-    return (
-      <div className={classes.root}>
-        <PercentageBars total={1} items={items} />
-        <PercentageBarsLegend items={items} />
-      </div>
-    );
-  },
-);
 
 export const ElectionTurnoutSection = themable<Props>(
   "ElectionTurnoutSection",
