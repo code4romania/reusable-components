e78b2b6ac7ca9822371bdc965c369b0462fec76c
Marius Petcu <dapetcu21@gmail.com>
2020-09-09 17:48:50 +0300

    Export new components

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,8 @@ export * from "./components/PartyResultCard/PartyResultCard";
 export * from "./components/PartyResultInline/PartyResultInline";
 export * from "./components/HorizontalStackedBar/HorizontalStackedBar";
 export * from "./components/PercentageBars/PercentageBars";
+export * from "./components/PercentageBarsLegend/PercentageBarsLegend";
+export * from "./components/BarChart/BarChart";
+export * from "./components/ElectionMap/ElectionMap";
 export * from "./components/ElectionTurnoutSection/ElectionTurnoutSection";
+export * from "./components/ElectionObservationSection/ElectionObservationSection";
