8e999b94094630dd7e61c404a5854347b4b6eb25
Marius Petcu <dapetcu21@gmail.com>
2020-09-14 15:23:57 +0300

    Remove space between value and percent on Safari

--- src/util/format.ts
+++ src/util/format.ts
@@ -4,7 +4,9 @@ export const formatPercentage = (x: number): string =>
   new Intl.NumberFormat("ro-RO", {
     style: "percent",
     maximumFractionDigits: 2,
-  }).format(x);
+  })
+    .format(x)
+    .replace(/\s+%$/, "%");
 
 export const formatGroupedNumber = (x: number): string =>
   new Intl.NumberFormat("ro-RO", {
