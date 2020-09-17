4b232af12ec785ec32c73e40da3badc793236239
Marius Petcu <dapetcu21@gmail.com>
2020-09-17 20:46:52 +0300

    Use same protocol as page for locally-hosted maps
    So that we can test maps on the Vercel deployment until
    we have a more permanent URL.

--- src/constants/servers.ts
+++ src/constants/servers.ts
@@ -1,3 +1,3 @@
 // TODO
 export const electionApiProductionUrl = "https://api.rezultatevot.ro/api";
-export const electionMapOverlayUrl = "http://localhost:5000";
+export const electionMapOverlayUrl = "//localhost:5000";
