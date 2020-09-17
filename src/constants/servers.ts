282a57eb35b53b7e328f3fd782e67e49b9a229bf
Marius Petcu <dapetcu21@gmail.com>
2020-09-17 20:36:10 +0300

    Change default API URL

--- src/constants/servers.ts
+++ src/constants/servers.ts
@@ -1,3 +1,3 @@
 // TODO
-export const electionApiProductionUrl = "https://example.com";
+export const electionApiProductionUrl = "https://api.rezultatevot.ro/api";
 export const electionMapOverlayUrl = "http://localhost:5000";
