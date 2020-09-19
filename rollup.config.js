b68e9a4d8511e986a500694a31fe1acef6065404
Marius Petcu <dapetcu21@gmail.com>
2020-09-19 15:59:40 +0300

    Fix Rollup and NPM config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss";
 import image from "@rollup/plugin-image";
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
+import fs from "fs";
+import path from "path";
+
+// Treat all dependencies and peerDependencies as external (don't bundle them)
+// If you want something bundled, add it to devDependencies instead
+const pkg = JSON.parse(fs.readFileSync(path.resolve("./package.json"), "utf-8"));
+const external = Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {}));
 
 export default {
   input: {
@@ -11,12 +18,19 @@ export default {
   output: [
     {
       dir: "dist",
+      entryFileNames: "[name].mjs",
       format: "es",
+      sourcemap: true,
+    },
+    {
+      dir: "dist",
+      format: "cjs",
+      entryFileNames: "[name].cjs",
       sourcemap: true,
     },
   ],
   plugins: [
-    resolve(),
+    resolve({ browser: true }),
     image(),
     postcss({
       extract: false,
@@ -27,7 +41,8 @@ export default {
     typescript(),
     babel({
       exclude: "node_modules/**",
+      babelHelpers: "bundled",
     }),
   ],
-  external: ["react", "react-dom", "prop-types"],
+  external,
 };
