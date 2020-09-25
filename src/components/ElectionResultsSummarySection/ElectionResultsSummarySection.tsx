88f8b8624e10e0a6199bf915e2b62d0cf2b8bfef
Robert-Andrei Damian <damian.robert.andrei@gmail.com>
2020-09-25 16:02:32 +0200

    Changed message displayed for unavailable data

--- src/components/ElectionResultsSummarySection/ElectionResultsSummarySection.tsx
+++ src/components/ElectionResultsSummarySection/ElectionResultsSummarySection.tsx
@@ -90,7 +90,7 @@ export const ElectionResultsSummarySection = themable<Props>(
           loader
         ) : (
           <DivBodyHuge className={classes.warning}>
-            Nu există date despre prezența la vot pentru acest nivel de detaliu.
+            Nu există date disponibile pentru această unitate administrativ teritorială. Fie nu există date disponibile, fie câștigătorii pentru această unitate au fost aleși în primul tur de scrutin.
           </DivBodyHuge>
         ))}
       {results && <ElectionResultsStackedBar className={classes.stackedBar} results={results} />}
