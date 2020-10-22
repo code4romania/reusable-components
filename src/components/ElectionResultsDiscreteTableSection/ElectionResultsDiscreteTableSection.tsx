fcd7e610dc97707c742d2b046f7ee49fe8cac1ac
RaduCStefanescu <stefanescuradu@gmail.com>
2020-10-22 13:30:10 +0300

    Add toggle to discreet table

--- src/components/ElectionResultsDiscreteTableSection/ElectionResultsDiscreteTableSection.tsx
+++ src/components/ElectionResultsDiscreteTableSection/ElectionResultsDiscreteTableSection.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { ResultsTable } from "../ResultsTable/ResultsTable";
 import { ElectionResultsCandidate } from "../../types/Election";
 import { formatGroupedNumber } from "../../util/format";
 import { themable } from "../../hooks/theme";
+import { Button } from "../Button/Button";
 import classes from "./ElectionResultsDiscreteTableSection.module.scss";
 
 type Props = {
@@ -14,6 +15,12 @@ const CandidateTable: React.FC<{
   candidates: ElectionResultsCandidate[] | undefined;
   heading: string;
 }> = ({ candidates, heading }) => {
+  const [collapsed, setCollapsed] = useState<boolean>(true);
+  const canCollapse = candidates && candidates.length >= 20;
+
+  const onToggleCollapsed = useCallback(() => {
+    setCollapsed((x) => !x);
+  }, []);
   return (
     <div className={classes.tableContainer}>
       <ResultsTable className={classes.table}>
@@ -25,14 +32,22 @@ const CandidateTable: React.FC<{
         </thead>
         <tbody>
           {candidates &&
-            candidates.map((candidate, index) => (
-              <tr key={index}>
-                <td className={classes.nameCell}>{candidate.name}</td>
-                <td>{formatGroupedNumber(candidate.votes)}</td>
-              </tr>
-            ))}
+            candidates.map(
+              (candidate, index) =>
+                (!(canCollapse && collapsed) || index < 10) && (
+                  <tr key={index}>
+                    <td className={classes.nameCell}>{candidate.name}</td>
+                    <td>{formatGroupedNumber(candidate.votes)}</td>
+                  </tr>
+                ),
+            )}
         </tbody>
       </ResultsTable>
+      {canCollapse && (
+        <Button className={classes.collapseButton} onClick={onToggleCollapsed}>
+          {collapsed ? "Afișează toate rezultatele" : "Ascunde toate rezultatele"}
+        </Button>
+      )}
     </div>
   );
 };
