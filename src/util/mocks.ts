1eed3495bcb7462807b69af718e0a61a2f1ae2c3
Ciprian Platica <platica.ciprian@gmail.com>
2020-11-21 17:08:42 +0200

    Fix mock for candidates route

--- src/util/mocks.ts
+++ src/util/mocks.ts
@@ -2399,7 +2399,7 @@ export const mockElectionAPI = makeElectionApi({
 
     [
       "GET",
-      "/ballots/1/candidates",
+      "/ballots/*/candidates",
       (async () => {
         await delay(1000);
         return mockCandidatesList;
