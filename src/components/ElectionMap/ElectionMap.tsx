61cb0bd592681737c67db04b09ab3958ad382181
Dinu-Iulian Popovici <dinu.iulian.popovici@gmail.com>
2024-06-06 18:51:18 +0300

    Update ElectionMap.tsx

--- src/components/ElectionMap/ElectionMap.tsx
+++ src/components/ElectionMap/ElectionMap.tsx
@@ -253,7 +253,13 @@ export const ElectionMap = themable<Props>(
             )
           )}
         </div>
-        <p>Sursa date: <a href="https://www.roaep.ro/" target="_blank" rel="noreferrer">Autoritatea Electorală Permanentă</a>.</p>
+        <p>
+          Sursa date:{" "}
+          <a href="https://www.roaep.ro/" target="_blank" rel="noreferrer">
+            Autoritatea Electorală Permanentă
+          </a>
+          .
+        </p>
       </div>
     );
   },
